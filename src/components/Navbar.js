b271002fb4509589f8e9e7694a8ef0ad8ba2ad73
JatuFaique <jatu631@gmail.com>
2022-04-03 22:27:22 +0530

    mockbee backend template

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,29 +2,10 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav class="top-nav center-align">
-      <div class="brand">
-        <a href="/index.html">
-          <img class="brand-logo" src="/ass" />
-        </a>
-        <span class="brand-name-title">The Office Bay</span>
-      </div>
-      <div>
-        <div>
-          <input type="text" name="" class="search-box" />
-        </div>
-      </div>
-      <div class="nav-icons">
-        <a href="/pages/authentication/authentication.html">
-          <i class="fas fa-user"></i>
-        </a>
-        <a href="/pages/wishlist/wishlist.html">
-          <i class="fas fa-heart"></i>
-        </a>
-        <a href="/pages/cart/cart.html">
-          <i class="fas fa-shopping-cart"></i>
-        </a>
-      </div>
-    </nav>
+    <div>
+      <nav>
+        <section></section>
+      </nav>
+    </div>
   );
 }
