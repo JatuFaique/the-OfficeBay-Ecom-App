886167050246ce939c83a624a7886c963f0fe55b
JatuFaique <jatu631@gmail.com>
2022-04-13 14:48:59 +0530

    created cart context

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { CartProvider } from "./context/CartProvider";
+import { BrowserRouter as Router } from "react-router-dom";
+import { cartReducer, initialState } from "./reducers/cartReducer";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
