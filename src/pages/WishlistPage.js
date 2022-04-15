e583aac4bef8dc447b7194b3339b37959a574acf
JatuFaique <jatu631@gmail.com>
2022-04-15 11:40:37 +0530

    created wishlistpage

--- src/pages/WishlistPage.js
+++ src/pages/WishlistPage.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function WishlistPage() {
-  return <div>WishlistPage</div>;
+  return (
+    <div className="section__wishlist p-3">
+      <div class="text-l text-center">Your Wishlist</div>
+      <div class="text-sm text-grey text-center">
+        You can find your wishlisted items here!
+      </div>
+      <div className="section__wishlistItems">Products here</div>
+    </div>
+  );
 }
 
 export default WishlistPage;
